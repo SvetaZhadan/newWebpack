7ba978151396e09c5afb2c5a64113e08419352d5
SvetaZhadan <sveta.zhadan12@mail.ru>
2022-03-30 17:16:43 +0300

    check

--- app/js/index.js
+++ app/js/index.js
@@ -19,94 +19,125 @@ import accordion from './plugins/accordion';
 // ================================ Code ======================================
 const ripples = document.querySelectorAll('.--ripple');
 for (const ripple of ripples) {
-        new Ripple(ripple, { 
-                opacity:1
-         });
+  new Ripple(ripple, {
+    opacity: 1,
+  });
 }
 
-
-let header=document.querySelector('.m-header'),
-        headerMobile=document.querySelector('.m-headerMenu'),
-        logo=document.querySelector('.m-header__logo'),
-        burger=document.querySelector('.b-burger'),
-        menuItem=headerMobile.querySelectorAll('.b-textLink ');
-
-        console.log(menuItem);
+let header = document.querySelector('.m-header'),
+  headerMobile = document.querySelector('.m-headerMenu'),
+  logo = document.querySelector('.m-header__logo'),
+  burger = document.querySelector('.b-burger'),
+  menuItem = headerMobile.querySelectorAll('.b-textLink ');
 
 for (let index = 0; index < menuItem.length; index++) {
-        menuItem[index].onclick=function(){
-                burger.classList.remove('--close') 
-                document.body.classList.remove('--no-scroll')
-                headerMobile.classList.remove('--active')
-                header.classList.remove('--h-100per')
+  menuItem[index].onclick = function () {
+    burger.classList.remove('--close');
+    document.body.classList.remove('--no-scroll');
+    headerMobile.classList.remove('--active');
+    header.classList.remove('--h-100per');
 
-                let scrolled = window.pageYOffset;
-                if (scrolled < 10) {
-                        header.classList.remove('--bg-white')    
-                        header.classList.remove('--h-100per')
-                        logo.classList.add('--white')
-                }
-        }
-        
+    let scrolled = window.pageYOffset;
+    if (scrolled < 10) {
+      header.classList.remove('--bg-white');
+      header.classList.remove('--h-100per');
+      logo.classList.add('--white');
+    }
+  };
 }
 
+function toggleMenu(obj) {
+  document.body.classList[obj]('--no-scroll');
+  burger.classList[obj]('--close');
+  headerMobile.classList[obj]('--active');
+  header.classList[obj]('--h-100per');
+  header.classList[obj]('--bg-white');
+}
 
-burger.onclick=function() {
-        //onclose
-        if(burger.classList.contains('--close')) {
-                let scrolled = window.pageYOffset;
-                burger.classList.remove('--close') 
-                document.body.classList.remove('--no-scroll')
-                headerMobile.classList.remove('--active')
-                header.classList.remove('--h-100per')
-                
-                if (scrolled < 10) {
-                        header.classList.remove('--bg-white')    
-                        header.classList.remove('--h-100per')
-                        logo.classList.add('--white')
-                }
-        }
-        //onopen
-        else {
-                burger.classList.add('--close') 
-                document.body.classList.add('--no-scroll')
-                headerMobile.classList.add('--active')
-                header.classList.add('--bg-white')
-                header.classList.add('--h-100per')
-                logo.classList.remove('--white')
-
-                headerMobile.scrollTo({
-                        top:0,
-                        behavior: "smooth"
-                })
-        }
+burger.onclick = function () {
+  //onclose
+  if (burger.classList.contains('--close')) {
+    
+    toggleMenu('remove');
+    logo.classList.add('--white');
+  }
+  //onopen
+  else {
+    toggleMenu('add');
+    logo.classList.remove('--white');
+    headerMobile.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }
 };
 
-window.onscroll = function() {
-let scrolled = window.pageYOffset;
-        // console.log( 'Позиция скрола: '+scrolled );
-        if (scrolled >= 10) {
-                header.classList.add('--bg-white') 
-                logo.classList.remove('--white')
-        }
+let btnCheckPopop = document.querySelector('.needMore'),
+  checkPopup = document.querySelector('.c-check-popup'),
+  constructor = document.querySelector('.m-constructor');
 
-        if (scrolled < 10){ 
-                header.classList.remove('--bg-white')
-                logo.classList.add('--white')
-        }
+btnCheckPopop.onclick = function () {
+  if (checkPopup.classList.contains('--close')) {
+    checkPopup.classList.add('--open');
+    checkPopup.classList.remove('--close');
+    document.body.classList.add('--no-scroll');
+  } else {
+    checkPopup.classList.remove('--open');
+    checkPopup.classList.add('--close');
+    document.body.classList.remove('--no-scroll');
+  }
 };
 
-let btnCheckPopop=document.querySelector('.needMore'),
-        checkPopup=document.querySelector('.c-check-popup'),
-        constructor=document.querySelector('.m-constructor');
+window.onscroll = function () {
+  let scrolled = window.pageYOffset;
+  // console.log( 'Позиция скрола: '+scrolled );
+  if (scrolled >= 10) {
+    header.classList.add('--bg-white');
+    logo.classList.remove('--white');
+  }
 
-btnCheckPopop.onclick=function(){
-        if(checkPopup.classList.contains('--close')) {
-                checkPopup.classList.add('--h-100per');
-                checkPopup.classList.remove('--close');
-        }
-        else{
-                checkPopup.classList.remove('--h-100per');
-                checkPopup.classList.add('--close');
-        }
+  if (scrolled < 10) {
+    header.classList.remove('--bg-white');
+    logo.classList.add('--white');
+  }
+
+  let windowScroll = window.scrollY + window.innerHeight,
+    constructopOfTop = constructor.offsetTop;
+  if (windowScroll > constructopOfTop && windowScroll < constructopOfTop + constructor.clientHeight) {
+    checkPopup.classList.add('--h-100px');
+  } else {
+    checkPopup.classList.remove('--h-100px');
+  }
+};
+
+let closefoodCarts = document.querySelector('.closefoodCartsFirst'),
+  foodFirstPopup = document.querySelector('.m-foodCartsFirst'),
+  blackOut = document.querySelector('.blackout');
+  console.log('test 1');
+
+closefoodCarts.onclick = function () {
+  popupVisible('remove', foodFirstPopup);
+};
+console.log('test 2');
+
+blackOut.onclick = function () {
+  popupVisible('remove', foodFirstPopup);
+};
+console.log('test 3');
+
+let fitstCourseBtn = document.querySelector('.firstСourse'),
+  secondCourseBtn = document.querySelector('.seondСourse'),
+  sladCourseBtn = document.querySelector('.saladСourse');
+
+fitstCourseBtn.onclick = function () {
+  console.log('test');
+  let set;
+  set = foodFirstPopup.classList.contains('--open') ? 'remove' : 'add';
+  popupVisible(set, foodFirstPopup);
+};
+
+function popupVisible(obj, popup) {
+  popup.classList[obj]('--open');
+  blackOut.classList[obj]('--open');
+  document.body.classList[obj]('--no-scroll');
 }
